5147277839e437a8428271306132dee9f41e8d27
Zapania Hope Kanyika <95310342+bed-com-40-19@users.noreply.github.com>
2024-06-11 22:33:09 +0200

    This is the agritrading section

---
+++ AGRITRADING/soyabeans.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Soya Beans</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="aboutus.css" rel="stylesheet">
+  <link href="body.css" rel="stylesheet">
+  
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384"></script>
+
+
+  <!-- Favicons -->
+  <link href="images/Logo.png" rel="icon">
+  <link href="images/Logo.png" rel="apple-touch-icon">
+
+</head>
+<body>
+<?php include ("nav.php");  ?>
+<div class="main-head ">
+    <br>
+                 <h2 style="color:green; font-size: 20 px;">Soy Beans</h2>  
+            </div>
+          </div>
+          </div>
+          <div class="row">
+          
+            <div class="col-md-7 mar-top-30"> 
+            
+            <div class="mar-top-0"> 
+               <!-- <h4>Details</h4>   -->
+                </div>
+                 
+             <div class="mar-top-0">       
+           <!-- <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>-->
+          
+          <table width="100%">
+            <thead>
+              <tr>
+                    <th>Site</th>
+                    <th>Quantity (Kgs)</th>
+              </tr>
+            </thead>
+            <tbody>
+                            <tr>
+                <td>Chikwina / Mpamba AP</td>
+                <td>48590</td>
+              </tr>
+                            <tr>
+                <td>Ntchisi/Nthondo AP</td>
+                <td>563255</td>
+              </tr>
+                            <tr>
+                <td>Nkhoma Chilenje AP</td>
+                <td>355676</td>
+              </tr>
+                            <tr>
+                <td>Lilongwe</td>
+                <td>28000</td>
+              </tr>
+                          </tbody>
+          </table> 
+          </div>      
+            </div>
+            
+            <div class="col-md-5 mar-top-30"> 
+        <div  class="slider-for slider"> 
+            <div>
+              <img style="max-height: 160px" src="https://emlimi.digital/uploads/soya_beans.jpg" alt="eMlimi"> 
+            </div>
+        </div>
+         <!-- 
+          <div  class="slider-nav slider">
+            <div>
+              <img src="images/product/shop_img_big_01.jpg" alt="eMlimi"> 
+            </div>
+            <div>
+            <img src="images/product/shop_img_big_02.jpg" alt="eMlimi"> 
+              
+            </div>
+            <div>
+             <img src="images/product/shop_img_big_03.jpg" alt="eMlimi"> 
+               
+            </div>
+             
+          
+          </div>
+     -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="agritrading-bottom">
+      <div class="container">
+        <div class="agritrading-bottom-in">
+
+          
+
+<form class="form-horizontal form-row" action="https://emlimi.digital/eng/agritrading/soy_beans" accept-charset="utf-8" method="post">   <h2 style="width: 100%" >Interested? <span>Place order</span></h2>
+
+		     
+		    <div class="col-md-4 mar-top-20">               
+               <div class="mar-top-10 form-group">
+               	 <h5>Number of Kgs Required</h5>
+                 <input class="form-control input-md" placeholder="Kgs Required" name="quantity" value="" type="text" id="form_quantity" /> 
+                 </div>
+             </div>
+              <div class="col-md-4 mar-top-20">               
+               <div class="mar-top-10 form-group">
+               	 <h5>Preffered price (MKW)</h5>
+                 <input class="form-control input-md" placeholder="" name="price" value="" type="text" id="form_price" /> 
+                 </div>
+             </div>
+              <div class="col-md-4 mar-top-20">               
+               <div class="mar-top-10 form-group">
+               	 <h5>Product to be delivered</h5>
+                 <select class="form-control input-md" placeholder="" name="delivery" id="form_delivery">
+	<option value="1">immediately</option>
+	<option value="2">In a weeks Time</option>
+	<option value="3">In a fortnight</option>
+</select> 
+                 </div>
+             </div>
+		      <div class="col-md-12">
+		    	<div class="form-group">
+		    		<h5>Additional Instructions</h5>
+		    		<textarea class="form-control" rows="8" placeholder="Message" name="message" id="form_message"></textarea> 			
+		    	</div>
+		    	 <input name="buyer_id" value="550" type="hidden" id="form_buyer_id" />		    	  <input name="crop_id" value="22" type="hidden" id="form_crop_id" />		    	  <input name="crop_name" value="Soy Beans" type="hidden" id="form_crop_name" />		    	   <input name="village_id" value="24" type="hidden" id="form_village_id" />		    </div>
+		       <div class="col-md-6">
+
+		    	<div class="form-group">
+		    		<input class="form-control input-md buy-now trs" name="submit" value="ORDER NOW" type="submit" id="form_submit" />	
+		    	</div>
+		    </div>
+
+</form>
+          </div>
+        </div>
+      </section>
+          <!-- end of agritrading details details section -->     <!-- </div> -->
+
+</body>
+</html>
